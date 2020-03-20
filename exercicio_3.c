b6c8a43a0d859880eb23eddd43482e9228631680
Kaique <kaiqueribeiro@alunos.utfpr.edu.br>
2020-03-19 22:23:47 -0300

    exercicio_3.c

---
+++ exercicio_3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define ENVER 1.6
+
+int main(void){
+    float altura, envergaduras, total;
+    
+    printf("\nQual a altura?\n");
+    scanf("%f", &altura);
+    printf("\nQuantas envergaduras?\n");
+    scanf("%f", &envergaduras);
+
+    altura = altura * ENVER;
+    total = envergaduras * altura;
+    total = total * 3;
+    printf("\nA area do terreno e: %2.f\n", total);
+    
+    return 0;
+}
