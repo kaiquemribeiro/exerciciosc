a294622bc598fe4eb02bf8fbc86b340271a26f43
kaiquemribeiro <kaiqueribeiro@alunos.utfpr.edu.br>
2020-03-21 15:34:51 -0300

    C

--- Lista_2/exercicio_2_lista_2.c
+++ Lista_2/exercicio_2_lista_2.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-    int 
+    float numero, aux;
+    scanf("%f", &numero);
+    if (numero <= 0){
+        aux = numero + 3;
+        printf("\n%2.f", numero);
+        printf("\n%2.f", aux);
+    }else{
+        aux = pow(numero,2);
+        aux = numero + 2*numero;
+        printf("\n%2.f", numero);
+        printf("\n%2.f", aux);
+    }
+    
+    
     return 0;
 }
